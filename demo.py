18d42bb610e76bf1227204a40f3e613257702c9d
Matias <matias@brandon.(none)>
2014-04-17 10:14:36 +0200

    fixed bug with BGR to RGB conversion

--- demo.py
+++ demo.py
@@ -16,13 +16,16 @@ WEBCAM_NEW_IMAGE = gevent.event.Event()
 def _do_webcam_acq():
     global WEBCAM_ENCODED_JPEG
     webcam = cv2.VideoCapture(0)
+
     while True:
         _, frame = webcam.read()
         h,w,depth = frame.shape
+        im = Image.fromarray(frame) 
         # too bad it has to be converted from BGR to RGB :(
-        im = Image.fromarray(numpy.roll(frame, 1, axis=-1)) 
+        b,g,r = im.split()
+        rgb_im = Image.merge("RGB", (r,g,b))
         jpeg_file = cStringIO.StringIO()
-        im.save(jpeg_file, format="JPEG")
+        rgb_im.save(jpeg_file, format="JPEG")
         WEBCAM_ENCODED_JPEG = base64.b64encode(jpeg_file.getvalue())
         WEBCAM_NEW_IMAGE.set()
         WEBCAM_NEW_IMAGE.clear()
@@ -36,7 +39,6 @@ def start_webcam_acq():
 @bottle.route("/")
 def demo_app():
     start_webcam_acq()
-
     return bottle.static_file("demo.html", root=os.path.dirname(__file__))
 
 @bottle.route("/stream")
@@ -44,9 +46,7 @@ def send_jpeg_stream():
     bottle.response.content_type = "text/event-stream"
     bottle.response.connection = "keep-alive"
     bottle.response.cache_control = "no-cache"
-    frame_no = 0
     while True:
         WEBCAM_NEW_IMAGE.wait()
-        frame_no += 1
         yield "data: %s\n\n" % WEBCAM_ENCODED_JPEG
 
